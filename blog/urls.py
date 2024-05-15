ee45e37e1117fdd1a0a0ff962ceae4a6f03abeaf
SorathiyaSwaraj <127015764+SorathiyaSwaraj@users.noreply.github.com>
2024-05-15 07:28:44 +0530

    added login and signup

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,10 @@ urlpatterns = [
     path('blog',views.blog,name="blog"),
     path('contact',views.contact,name="contact"),
     path('category',views.category,name="category"),
-    path('<str:slug>/',views.single,name="single"),
-    # path('<str:int>/',views.single,name="single"),
+    path('extra/<str:slug>/',views.single,name="single"),
+    path('<str:sno>/',views.my_single,name="my_single"),
+    path('signupForm_view', views.signupForm_view, name="signupForm_view"),
+    path('login_view', views.login_view, name="login_view"),
+    path('logout_view', views.logout_view, name="logout_view"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 
@@ -125,4 +126,87 @@ def single(request,slug):
 
 
 
+def my_single(request,sno):
+	home = Home.objects.all()
+	cate = Category.objects.all()
+	post = Post.objects.filter(sno=sno).first()
 
+	# comment = Comment.objects.filter(post=post)
+	
+
+	# if request.method == "POST":
+	# 	name = request.POST['name']
+	# 	email = request.POST['email']
+	# 	postsno = request.POST.get('postsno')
+	# 	website = request.POST['website']
+	# 	message = request.POST['message']
+
+	# 	post_data = Post.objects.get(sno=postsno)
+		
+	# 	comment = Comment(name=name,email=email,post=post_data,website=website,message=message)
+	# 	comment.save()
+	# 	messages.success(request,"Comment Successfully Submit")
+
+	# 	return redirect('/')
+
+
+	context = {'home':home,'cate':cate,'post':post}
+	return render(request,'blog/single.html',context)
+
+
+
+def signupForm_view(request):
+    
+    if request.method == "POST":
+        
+        print("Welcomm to sign up page")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+        phone_no = request.POST.get("phone_no")
+        
+        # Check if the user already exists
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, 'User already exists. Please log in.')
+            # return redirect("/login_view")      
+        
+        else:
+            user = CustomUser.objects.create_user(username, email, password, phone_no = phone_no)
+            
+            print(user)
+            if user is not None:
+                # A backend authenticated the credentials
+                login(request, user)
+                return redirect("/home")
+            else:
+                # No backend authenticated the credentials
+                return redirect("/signupForm_view")
+
+    return render(request, "signup.html")
+
+
+
+def login_view(request):
+
+    if request.method == "POST":
+       
+        print("Welcomm to login page is valid")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/home")
+        else:
+            # No backend authenticated the credentials
+            messages.error(request, 'User not exists. Please Sign up.')
+
+    return render(request, "login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/home")
